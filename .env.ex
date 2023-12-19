e33807245b60e4e517d55dd28402b04043858b4b
John Hanson <nubzzz@nubzzz.com>
2023-12-18 18:57:37 -0600

    Adding in an example env file

---
+++ .env.ex
@@ -0,0 +1,7 @@
+PUSHOVER_API_KEY="api_key"
+PUSHOVER_USER_KEY="user_key"
+S3_HOST="http://localhost:9000"
+S3_BUCKET="media"
+AWS_KEY="access-key"
+AWS_SECRET_KEY="secret-key"
+UPPER_BOUND_BITRATE="8388608"
